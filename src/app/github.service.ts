eb4914e84dda6d2619d17064b3699c0c1e5f8b72
Nitin Kale <nitinkale.nk@gmail.com>
2018-08-20 18:45:24 +0530

    components skeleton ready

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -26,12 +26,14 @@ export class GithubService {
     }).pipe(catchError(this.handleError));
   }
 
+  //https://api.github.com/repos/nitinkalenk/snippets/contents/Angular/kudvenkatAngularCrud/angular.json?ref=master
+  //https://github.com/nitinkalenk/snippets/contributors/master/Angular/kudvenkatAngularCrud/src/app/employees/create-employee/create-employee.component.ts
   getFileContent(): Observable<any> {
     console.log(this.httpHeaders);
-    return this.http.get('https://api.github.com/repos/nitinkalenk/snippets/contents/Angular/kudvenkatAngularCrud/angular.json?ref=master',{
+    return this.http.get('https://api.github.com/repos/nitinkalenk/snippets/contents/Angular/kudvenkatAngularCrud/src/app/app.component.ts?ref=master',
+    {
       headers : this.httpHeaders
-    })
-      .pipe(catchError(this.handleError));
+    }).pipe(catchError(this.handleError));
   }
 
   private handleError(error: HttpErrorResponse) {
